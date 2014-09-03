7d314f32142f45e492cb8f336a822fcfae775eb3
pmurcia <p.murcia.morilla@gmail.com>
2014-09-03 18:22:34 +0000

    Index of courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,13 @@
 class CoursesController < ApplicationController
+  # before_filter :set_subject
   def index
+  	@subject = Subject.find params[:subject_id]
+  	@courses = @subject.courses.all
+  end
 
+  private
+
+  def set_subject
+  	subject = Subject.find params[:subject_id]
   end
 end
