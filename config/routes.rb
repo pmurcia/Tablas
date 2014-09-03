704750e581bd5880fc33b00eea08329c54161ead
pmurcia <p.murcia.morilla@gmail.com>
2014-09-03 16:02:12 +0000

    Renamed as courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'subjects#index'
-  resources :subjects
+  resources :subjects do
+    resources :courses
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
